38bc8119fa0d807e16613e7ac75f2f5c04a06c22
adrianca6693 <163523621+adrianca6693@users.noreply.github.com>
2024-10-23 19:21:06 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,103 @@
+import oracledb
+import os
+import sys
+from datetime import datetime
+# Función para obtener las credenciales de un archivo externo "config.ini"
+# El formato de este archivo es el siguiente:
+#<user>
+#<password>
+#<dsn>
+def get_credentials():
+    try:
+        with open('config.ini', 'r') as file:
+            user = file.readline().strip()
+            password = file.readline().strip()
+            dsn = file.readline().strip()
+    except FileNotFoundError as errorFNFE:  #Error al abrir el archivo
+        print("Error reading the file: ", errorFNFE)
+    finally:
+        file.close()
+    return [user, password, dsn]
+
+#Esta función limpia la pantalla de la consola, para cuando se haga el menú
+def clear():
+    if(os.name == 'nt'):    #Si el sistema es windows se usa el comando cls si es otro se usa clear
+        os.system('cls')
+    else:
+        os.system('clear')
+        
+def opcion1(cursor):
+    print("Pendiente de implementar.", file=sys.stderr)
+    
+def opcion2(cursor):
+    try:
+        Cpedido= int(input("Codigo del pedido: "))
+        Ccliente = int(input("Codigo del cliente: "))
+        fecha = input("Introduce una fecha (formato dd/mm/aaaa): ")
+        fecha2 = datetime.strptime(fecha, "%d/%m/%Y")
+        Fecha_pedido = fecha2.strftime("%Y-%m-%d")
+        cursor.execute("INSERT INTO Pedido (Cpedido, Ccliente, Fecha_pedido) VALUES (?,?,?)",(Cpedido,Ccliente,Fecha_pedido))
+        #print("cpedido ", Cpedido, "ccliente ", Ccliente , "fecha ", Fecha_pedido)
+        
+        #print("Pendiente de implementar.", file=sys.stderr)
+
+
+    except oracledb.DatabaseError as errorBD:
+        error = errorBD.args[0]
+        print("Error in database operation: ", error.message)
+        print("Error code: ", error.code)
+        cursor.connection.rollback()  # Revertir cambios en caso de error
+    except Exception as otroError:
+        print("Another error: ", otroError)
+        cursor.connection.rollback()
+
+   
+    
+def opcion3(cursor):
+    print("Pendiente de implementar.", file=sys.stderr)
+    
+def opcion4(cursor):
+    return
+
+def imprimir_menu():
+    espaciado = 46
+    clear()
+    print("=" * espaciado)
+    print("               MENÚ INTERACTIVO")
+    print("=" * espaciado)
+    print("[1] Borrado y creación de tuplas predefinidas.")
+    print("[2] Dar de alta nuevo pedido.")
+    print("[3] Mostrar contenido de las tablas.")
+    print("[4] Salir.")
+    print("=" * espaciado)
+    
+def menu(cursor):
+    funciones = [opcion1,opcion2,opcion3,opcion4]
+    imprimir_menu()
+    opcion = int(input("Introduzca un número del 1 al 4: "))
+    while opcion not in range(1,5):
+        print("Debe introducir un número entre el 1 y el 4", file=sys.stderr)
+        opcion = int(input())
+    funciones[opcion-1](cursor)
+4
+def main():
+    try: 
+        user, password, dsn = get_credentials()
+        conexion = oracledb.connect(user=user, password=password, dsn=dsn)
+        cursor = conexion.cursor()
+        # Solicitudes y resto de codigo aquí
+        menu(cursor)
+    except oracledb.DatabaseError as errorBD:   #Error al establecer la conexión de la base de datos
+        error = errorBD.args[0]
+        print("Error connecting to the database: ", error.message)
+        print("Error code: ", error.code)
+    except KeyboardInterrupt:   #Si hacemos control + c
+        print("\nSaliendo con Ctrl+C...")
+    except Exception as otroError:  #Otro tipo de error
+        print("Another error: ", otroError)
+    finally:    #Al final, pase lo que pase se cierran el cursor y la conexión
+        cursor.close()
+        conexion.close()
+
+if __name__ == "__main__":
+    main()
